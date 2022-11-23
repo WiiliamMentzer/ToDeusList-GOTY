36e4488b31313437187befe51619bf7f81691e86
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-11-22 19:01:05 -0800

    v0.1, Added title screen, stats for enemy and player, WIP title screen lightning. Init.

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public string playerName;
+
+    public int playerMaxHP;
+    public int playerHP;
+
+    public int playerSpeed;
+    public int playerAttack;
+
+}
---
+++ Assets/Scripts/ToDoList/ToDoListStats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToDoListStats : MonoBehaviour
+{
+    public string toDoListName;
+
+    public int toDoListMaxHP;
+    public int toDoListHP;
+
+    public int toDoListAttack;
+}
